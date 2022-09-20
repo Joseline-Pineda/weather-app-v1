2ffec347491a49deb0dab5aee3d0e74b4be50234
Joseline-Pineda <joseline.pineda@catolica.edu.sv>
2022-09-20 14:25:15 -0600

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-container class="fill-height py-0" fluid
+    ><v-row class="fill-height"
+      ><v-col class="primary" md="3"><v-row class="sidebar"></v-row></v-col
+      ><v-col class="secondary"></v-col></v-row
+  ></v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
+  name: "Home",
 };
 </script>
+<style scoped>
+.sidebar {
+  background-image: url(@/assets/img/Cloud-background.png);
+  background-size: 45rem;
+  background-position: 45% 15%;
+  filter: grayscale(200%) opacity(0.1) drop-shadow(0 0 0 black);
+  height: 50%;
+}
+</style>
