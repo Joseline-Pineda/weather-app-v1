92dec744c727a8e4faf4b00aac2337bc2124c0d3
Joseline-Pineda <joseline.pineda@catolica.edu.sv>
2022-09-20 15:44:13 -0600

    agregando WeatherWidget component

---
+++ src/components/WeatherWidget.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-card class="mx-auto primary" max-width="160">
+    <v-list-item two-line>
+      <v-list-item-content>
+        <v-list-item-title class="text-h6 text-center">
+          Mon, 8 Jun
+        </v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-card-text>
+      <v-row>
+        <v-col cols="12" offset-md="2" md="8">
+          <v-img src="@/assets/img/LightCloud.png" alt="Sunny image"></v-img>
+        </v-col>
+      </v-row>
+    </v-card-text>
+    <v-card-actions>
+      <v-list-item class="font-weight-bold" text> 16°C </v-list-item>
+      <v-list-item text> 11°C </v-list-item>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      labels: ["SU", "MO", "TU", "WED", "TH", "FR", "SA"],
+      time: 0,
+      forecast: [
+        {
+          day: "Tuesday",
+          icon: "mdi-white-balance-sunny",
+          temp: "24\xB0/12\xB0",
+        },
+        {
+          day: "Wednesday",
+          icon: "mdi-white-balance-sunny",
+          temp: "22\xB0/14\xB0",
+        },
+        { day: "Thursday", icon: "mdi-cloud", temp: "25\xB0/15\xB0" },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,19 +2,23 @@
   <v-container class="fill-height py-0" fluid
     ><v-row class="fill-height"
       ><v-col class="primary" md="3"><v-row class="sidebar"></v-row></v-col
-      ><v-col class="secondary"></v-col></v-row
+      ><v-col class="secondary"><WeatherWidget /></v-col></v-row
   ></v-container>
 </template>
 
 <script>
+import WeatherWidget from "@/components/WeatherWidget.vue";
 export default {
   name: "Home",
+  components: {
+    WeatherWidget,
+  },
 };
 </script>
 <style scoped>
 .sidebar {
   background-image: url(@/assets/img/Cloud-background.png);
-  background-size: 45rem;
+  background-size: 35rem;
   background-position: 45% 15%;
   filter: grayscale(200%) opacity(0.1) drop-shadow(0 0 0 black);
   height: 50%;
